7e84a03354fba0095e37a17baf2c8102c6d7eef5
Mukesh <91588183+tsMUKESH@users.noreply.github.com>
2023-06-28 11:26:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Header from './components/Header';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <Router>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/ColorModeSwitcher.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useColorMode, useColorModeValue, IconButton } from '@chakra-ui/react';
+import { FaMoon, FaSun } from 'react-icons/fa';
+
+export const ColorModeSwitcher = props => {
+  const { toggleColorMode } = useColorMode();
+  const text = useColorModeValue('dark', 'light');
+  const SwitchIcon = useColorModeValue(FaMoon, FaSun);
+
+  return (
+    <IconButton
+      aria-label={`Switch to ${text} mode`}
+      variant="ghost"
+      color="current"
+      pos={"fixed"}
+      top={"4px"}
+      right={"4px"}
+      onClick={toggleColorMode}
+      icon={<SwitchIcon />}
+      {...props}
+    />
+  );
+};
